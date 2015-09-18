734c0e7844e53e6f9f9304575a8e601f1756a766
Kaur Kaljuma <kaurkal@greeny.cs.tlu.ee>
2015-09-18 13:28:42 +0300

    Tõmban nüüd asja endale kokku normaalselt

---
+++ Page/home.php
@@ -0,0 +1,15 @@
+<?php
+	$page_title = "Minu avaleht";
+	$file_name = "home.php";
+?>
+
+<?php
+
+//kopeerime header.php sisu
+//../ - tähistab, et fail asub 1 võrra ülamkaustas
+require_once("../header.php")
+?>
+
+	<h2>Avaleht</h2>
+
+<?php require_once("../footer.php"); ?>
--- Page/login.php
+++ Page/login.php
@@ -5,46 +5,73 @@
 	//Defineerime muutujad
 	$email_error = "";
 	$password_error = "";
+	$name_error = "";
 	
 	//kontrollin kas keegi vajutas nuppu
 	if($_SERVER["REQUEST_METHOD"] == "POST") {
 		
-		//echo "jah";
 		
-		// kas e-post on tühi
-		if( empty($_POST["email"]) ) {
+		// kontrollin mis nuppu vajutati
+		if(isset($_POST["login"])){
 			
-			// jah oli tühi
-			$email_error = "See väli on kohustuslik";
+			// ********************
+			// *** LOGIN NUPP *****
+			// ********************
 			
-		}
+			// kas e-post on tühi
+			if( empty($_POST["email"]) ) {
+				
+				// jah oli tühi
+				$email_error = "See väli on kohustuslik";
+				
+			}
+			
+			// kas parool on tühi
+			if( empty($_POST["password"]) ) {
+				
+				// jah oli tühi
+				$password_error = "See väli on kohustuslik";
+				
+			}
 		
-		// kas parool on tühi
-		if( empty($_POST["password"]) ) {
+		
+		} elseif(isset($_POST["create"])){
+		
+			// ********************
+			// *** CREATE NUPP ****
+			// ********************
 			
-			// jah oli tühi
-			$password_error = "See väli on kohustuslik";
-			
+			// kas e-post on tühi
+			if( empty($_POST["name"]) ) {
+				
+				// jah oli tühi
+				$name_error = "See väli on kohustuslik";
+				
+			}
+		
+		
 		}
 		
 	}
 
 ?>
-<html>
-<head>
-	<title>Login page</title>
-</head>
-<body>
-	
+<?php
+	$page_title = "Login leht";
+	$file_name = "login.php";
+?>
+<?php require_once("../header.php"); ?>
 	<h2>Login</h2>
 	<form action="login.php" method="post">
 		<input name="email" type="email" placeholder="E-post" > <?php echo $email_error; ?><br><br>
 		<input name="password" type="password" placeholder="Parool" > <?php echo $password_error; ?> <br><br>
-		<input type="submit" value="Logi sisse" > <br><br>
+		<input name="login" type="submit" value="Logi sisse" > <br><br>
 	</form>
 	
 	
 	<h2>Create user</h2>
-	
-</body>
-</html>
+	<form action="login.php" method="post">
+		<input name="name" type="text" placeholder="Eesnimi Perenimi" > <?php echo $name_error; ?><br><br>
+		<input name="create" type="submit" value="Loo kasutaja" > <br><br>
+	</form>
+	 
+<?php require_once("../footer.php"); ?>
